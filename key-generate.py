80632b5da42371b97a7f51e23f0071b95e25bd46
EtienneSelem <etienneselemani47@gmail.com>
2023-02-14 01:24:19 +0100

    second commit

--- key-generate.py
+++ key-generate.py
@@ -1,4 +1,6 @@
 import numpy as np
+from pyrsistent import b
+
 
 def isprem(n):
     """retourne True si n est premier, False dans le cas contraire.
@@ -6,7 +8,7 @@ def isprem(n):
 
     if n == 1 or n == 2:
       return True
-      if n%2 == 0:
+      if n % 2 == 0:
         return False
 
         r = n**0.5
@@ -24,4 +26,32 @@ def isprem(n):
 
         d, f = 0, long
         l = []
-        
+
+        while f <= len(k):
+            l.append(k[d:f])
+            d, f = f, f + long
+
+        m = len(k)%long
+
+        if m != 0:
+          l.append(k[len(k)-m])
+
+          return l
+
+    def pgcd(a, b):
+        """retourne le plus grand dénominateur commun de a et b"""
+        while(b>0):
+            r = a%b
+            a, b = b, r
+
+            return   
+    
+    def pgcde(a, b):
+      """ pgcd étendu avec les 2 coefficients de bézout u et v
+	Entrée : a, b entiers
+	Sorties : r = pgcd(a,b) et u, v entiers tels que a*u + b*v = r
+	"""
+      r, u, v = a, 1, 0
+      rp, up, vp = b, 0, 1
+      
+  
